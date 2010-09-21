c44f1e8a514fb2d699ec8aeb0cb63083ace64754
Andy Grundman <andy@hybridized.org>
2010-09-21 19:56:18 +0000

    Simplify benchmark and use Dumbbench

--- tools/bench.pl
+++ tools/bench.pl
@@ -2,7 +2,7 @@
 
 use strict;
 
-use Benchmark qw(cmpthese);
+use Benchmark::Dumb qw(:all);
 use Image::Scale;
 use GD;
 
@@ -10,33 +10,19 @@ GD::Image->trueColor(1);
 
 my $path = shift || die "Path required\n";
 my $width = shift || die "Width required\n";
+my $saves = {};
 
 warn "Benchmarking $path resized to $width...\n";
 
-my $saves = {};
+my $jpg = $path =~ /jpe?g/ ? 1 : 0;
 
-my $make_gm = sub {
-    my ($filter, $type) = @_;
-    
-    return ( $filter => sub {
-        my $img = Image::Scale->new($path);
-        $img->resize_gm( { width => $width, filter => $filter } );
-        my $out = $type eq 'png' ? $img->as_png() : $img->as_jpeg();
-        if (!$saves->{"resize_gm_$filter"}++) {
-            open my $fh, '>', "resize_gm_${filter}.${type}";
-            print $fh $out;
-            close $fh;
-        }
-    } );
-};
-
-my $png = {
+my $tests = {
     resize_gd => sub {
         my $img = Image::Scale->new($path);
         $img->resize_gd( { width => $width } );
-        my $out = $img->as_png();
+        my $out = $jpg ? $img->as_jpeg() : $img->as_png();
         if ( !$saves->{resize_gd}++ ) {
-            open my $fh, '>', 'resize_gd.png';
+            open my $fh, '>', 'resize_gd.' . ($jpg ? 'jpg' : 'png');
             print $fh $out;
             close $fh;
         }
@@ -44,117 +30,39 @@ my $png = {
     resize_gd_fixed => sub {
         my $img = Image::Scale->new($path);
         $img->resize_gd_fixed_point( { width => $width } );
-        my $out = $img->as_png();
+        my $out = $jpg ? $img->as_jpeg() : $img->as_png();
         if ( !$saves->{resize_gd_fixed}++ ) {
-            open my $fh, '>', 'resize_gd_fixed_point.png';
+            open my $fh, '>', 'resize_gd_fixed_point.' . ($jpg ? 'jpg' : 'png');
             print $fh $out;
             close $fh;
         }
     },
-    #$make_gm->('Point', 'png'),
-    #$make_gm->('Box', 'png'),
-    $make_gm->('Triangle', 'png'),
-    #$make_gm->('Hermite', 'png'),
-    #$make_gm->('Hanning', 'png'),
-    #$make_gm->('Hamming', 'png'),
-    #$make_gm->('Blackman', 'png'),
-    #$make_gm->('Gaussian', 'png'),
-    #$make_gm->('Quadratic', 'png'),
-    #$make_gm->('Cubic', 'png'),
-    #$make_gm->('Catrom', 'png'),
-    #$make_gm->('Mitchell', 'png'),
-    #$make_gm->('Lanczos', 'png'),
-    #$make_gm->('Bessel', 'png'),
-    #$make_gm->('Sinc', 'png'),
-    gm_fixed => sub {
+    resize_gm => sub {
+        my $img = Image::Scale->new($path);
+        $img->resize_gm( { width => $width, filter => 'Triangle' } );
+        my $out = $jpg ? $img->as_jpeg() : $img->as_png();
+        if (!$saves->{"resize_gm"}++) {
+            open my $fh, '>', "resize_gm." . ($jpg ? 'jpg' : 'png');
+            print $fh $out;
+            close $fh;
+        }
+    },
+    resize_gm_fixed => sub {
         my $img = Image::Scale->new($path);
         $img->resize_gm_fixed_point( { width => $width } );
-        my $out = $img->as_png();
-        if ( !$saves->{resize_gm_fixed}++ ) {
-            open my $fh, '>', 'resize_gm_fixed_point.png';
-            print $fh $out;
-            close $fh;
-        }
-    },
-};
-
-if ( GD::Image->can('newFromPng') ) {
-    $png->{gd_resample} = sub {
-        my $src = GD::Image->newFromPng($path);
-        my $dst = GD::Image->new($width, $width);
-        $dst->saveAlpha(1);
-        $dst->alphaBlending(0);
-        $dst->filledRectangle(0, 0, $width, $width, 0x7f000000);
-        $dst->copyResampled(
-			$src,
-			0, 0,
-			0, 0,
-			$width, $width,
-			$src->width, $src->height
-		);
-		
-		my $out = $dst->png;
-		
-		if ( !$saves->{gd_resample}++ ) {
-    		open my $fh, '>', 'gd.png';
-    		print $fh $out;
-    		close $fh;
-    	}
-	};
-}
-
-my $jpg = {
-    resize_gd => sub {
-        my $img = Image::Scale->new($path);
-        $img->resize_gd( { width => $width } );
-        my $out = $img->as_jpeg();
-        if ( !$saves->{resize_gd}++ ) {
-            open my $fh, '>', 'resize_gd.jpg';
-            print $fh $out;
-            close $fh;
-        }
-    },
-    resize_gd_fixed => sub {
-        my $img = Image::Scale->new($path);
-        $img->resize_gd_fixed_point( { width => $width } );
-        my $out = $img->as_jpeg();
-        if ( !$saves->{resize_gd_fixed}++ ) {
-            open my $fh, '>', 'resize_gd_fixed_point.jpg';
-            print $fh $out;
-            close $fh;
-        }
-    },
-    #$make_gm->('Point', 'jpg'),
-    #$make_gm->('Box', 'jpg'),
-    $make_gm->('Triangle', 'jpg'),
-    #$make_gm->('Hermite', 'jpg'),
-    #$make_gm->('Hanning', 'jpg'),
-    #$make_gm->('Hamming', 'jpg'),
-    #$make_gm->('Blackman', 'jpg'),
-    #$make_gm->('Gaussian', 'jpg'),
-    #$make_gm->('Quadratic', 'jpg'),
-    #$make_gm->('Cubic', 'jpg'),
-    #$make_gm->('Catrom', 'jpg'),
-    #$make_gm->('Mitchell', 'jpg'),
-    #$make_gm->('Lanczos', 'jpg'),
-    #$make_gm->('Bessel', 'jpg'),
-    #$make_gm->('Sinc', 'jpg'),
-    gm_fixed => sub {
-        my $img = Image::Scale->new($path);
-        $img->resize_gm_fixed_point( { width => $width } );
-        my $out = $img->as_jpeg();
+        my $out = $jpg ? $img->as_jpeg() : $img->as_png();
         
         if ( !$saves->{resize_gm_fixed}++ ) {
-            open my $fh, '>', 'resize_gm_fixed_point.jpg';
+            open my $fh, '>', 'resize_gm_fixed_point.' . ($jpg ? 'jpg' : 'png');
             print $fh $out;
             close $fh;
         }
     },
 };
 
-if ( GD::Image->can('newFromJpeg') ) {
-    $jpg->{gd_resample} = sub {
-        my $src = GD::Image->newFromJpeg($path);
+if ( ($jpg && GD::Image->can('newFromJpeg')) || (!$jpg && GD::Image->can('newFromPng')) ) {
+    $tests->{gd_resample} = sub {
+        my $src = $jpg ? GD::Image->newFromJpeg($path) : GD::Image->newFromPng($path);
         my $dst = GD::Image->new($width, $width);
         $dst->copyResampled(
 			$src,
@@ -163,15 +71,15 @@ if ( GD::Image->can('newFromJpeg') ) {
 			$width, $width,
 			$src->width, $src->height
 		);
-		
-		my $out = $dst->jpeg(90);
-		
+
+		my $out = $jpg ? $dst->jpeg(90) : $dst->png;
+
 		if (!$saves->{gd_resample}++) {
-            open my $fh, '>', 'gd.jpg';
+            open my $fh, '>', 'gd.' . ($jpg ? 'jpg' : 'png');
             print $fh $out;
             close $fh;
         }
 	};
 }
 
-cmpthese( -5, $path =~ /png/ ? $png : $jpg );
+cmpthese( 0.005, $tests );
